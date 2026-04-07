48232bcb9b7ebc33c05362a20b63b4f40e5e3744
hollanddz228 <korol5056@gmail.com>
2026-04-08 02:09:50 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
-def hello_world():
-    # Просто печатаем текст, никаких входных данных и секретов
-    print("Hello, Jenkins! This is a safe script.")
-    return True
+import sqlite3
+
+def login_user(username, password):
+    # УЯЗВИМОСТЬ 1: Пароль прямо в коде (Hardcoded Credential)
+    admin_password = "super_secret_admin_password_123" 
+    
+    # УЯЗВИМОСТЬ 2: Формирование запроса через f-строку (SQL Injection)
+    # Хакер может ввести: admin' --
+    db = sqlite3.connect("users.db")
+    cursor = db.cursor()
+    query = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
+    
+    print(f"DEBUG: Checking credentials for {username}...")
+    cursor.execute(query)
+    return cursor.fetchone()
